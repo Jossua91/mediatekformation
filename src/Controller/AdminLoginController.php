fcebceb362a75e0faf4a2baff2a41efa7bdb13ff
Jossua <jossua.creuzet@gmail.com>
2025-04-27 13:53:27 +0200

    Deploy

--- src/Controller/AdminLoginController.php
+++ src/Controller/AdminLoginController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * Description of AdminLoginCOntroller
+ *
+ * @author emds
+ */
 class AdminLoginController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -6,7 +6,7 @@ use App\Repository\AdminRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
- 
+
 #[ORM\Entity(repositoryClass: AdminRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
 class Admin implements UserInterface, PasswordAuthenticatedUserInterface
